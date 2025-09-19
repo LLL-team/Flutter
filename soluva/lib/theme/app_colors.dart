008ced19722b6836cb76c3f4ca9673e994b165ad
Gerónimo Le Lan <42528358+GeroLeLan@users.noreply.github.com>
2025-09-19 11:28:54 -0300

    Update app_colors.dart

--- soluva/lib/theme/app_colors.dart
+++ soluva/lib/theme/app_colors.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF3B82F6); // azul principal
-  static const Color secondary = Color(0xFFF59E0B); // amarillo
-  static const Color background = Color(0xFFF1F5F9); // fondo claro
-  static const Color text = Color(0xFF1E293B); // texto principal
-  static const Color error = Color(0xFFEF4444);
-  static const Color button = Color.fromARGB(255, 67, 137, 218);
+  static const Color primary = Color.fromARGB(255,26,94,132); // azul principal
+  static const Color secondary = Color.fromARGB(255, 242, 115, 17); // amarillo
+  static const Color background = Color.fromARGB(255, 234, 230, 219); // fondo claro
+  static const Color text = Color.fromARGB(255, 16, 33, 47); // texto principal
+  static const Color button = Color.fromARGB(255, 89, 165, 165);
+
+
+  static const Color error = Color.fromARGB(255, 239, 68, 68);
+
   static const Color buttonText = Color.fromARGB(255, 2, 2, 2);
     // static const Color background = Color(0xFF121212);
     // static const Color primary = Color(0xFF6200EE);
