e6ad5693c448558f47567cb99e5c0678583c0cd0
Juan Manuel Luis <54417888+JuanMLuis@users.noreply.github.com>
2026-02-13 08:51:27 -0300

    feat(connection new endpont)
    The endpoint is not working because the MP connection is required. Next week, I'm going to talk with the girls

--- soluva/lib/services/mercadoPago_services/MP_service.dart
+++ soluva/lib/services/mercadoPago_services/MP_service.dart
@@ -17,7 +17,7 @@ Future<void> crearTransaccion({
   required String paymentMethodId,
 }) async {
   final prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('accessToken');
+  final token = prefs.getString('auth_token');
   final response = await http.post(
     Uri.parse("$baseUrl/transacciones/nueva"),
     headers: {
@@ -82,7 +82,7 @@ Future<String> generarCardToken({
 //se usa cuando tengamos la vinculacion con mercado pago, para verificar si el trabajador ya tiene una cuenta vinculada
 Future<bool> checkVinculacionMP() async {
   final prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('accessToken');
+  final token = prefs.getString('auth_token');
 
   final response = await http.get(
     Uri.parse("$baseUrl/mercadopago/verifyLinkedAccount"),
@@ -102,7 +102,7 @@ Future<bool> checkVinculacionMP() async {
 Future<void> conectarMercadoPago(String trabajadorUuid) async {
   //todo cuando actualizemos el vincular
   final prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('accessToken');
+  final token = prefs.getString('auth_token');
 
   final response = await http.get(
     Uri.parse("$baseUrl/mercadopago/connect/$trabajadorUuid"),
@@ -123,7 +123,7 @@ Future<void> conectarMercadoPago(String trabajadorUuid) async {
 Future<bool> desvincularMercadoPago() async {
   //todo cuando actualizemos el vincular
   final prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('accessToken');
+  final token = prefs.getString('auth_token');
 
   final response = await http.post(
     Uri.parse("$baseUrl/mercadopago/delete"),
@@ -137,4 +137,24 @@ Future<bool> desvincularMercadoPago() async {
     return false;
   }
   return true;
+}
+
+Future<List<dynamic>> obtenerMetodosDePago() async {
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('auth_token');
+
+  final response = await http.get(
+    Uri.parse("$baseUrl/mercadopago/payment-methods"),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer $token",
+    },
+  );
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    return data;
+  } else {
+    throw Exception('Error al obtener métodos de pago: ${response.body}');
+  }
 }
